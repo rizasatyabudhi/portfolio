f5fffb02bfd5ac76a2c96b7c349a5946c349cdc3
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-10-10 09:10:09 +0700

    scss configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Student Games`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`]
+};
